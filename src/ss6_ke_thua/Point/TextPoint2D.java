97166e1c0df7af3c480b5112506ebeaa147e695e
anhquoc98 <anhquoc98dn@gmail.com>
2022-12-09 22:39:14 +0700

    main

---
+++ src/ss6_ke_thua/Point/TextPoint2D.java
@@ -0,0 +1,9 @@
+package ss6_ke_thua.Point;
+
+public class TextPoint2D {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(3, 7);
+        System.out.println(point2D);
+
+    }
+}
---
+++ src/ss6_ke_thua/Point/TextPoint3D.java
@@ -0,0 +1,8 @@
+package ss6_ke_thua.Point;
+
+public class TextPoint3D {
+    public static void main(String[] args) {
+        Point3D point3D=new Point3D(3,5,5);
+        System.out.println(point3D);
+    }
+}
